0c19a84d9c8c645ead324e24ffef6c0cefcec310
Quadrox-Tech <mukhtaradeshinaabdulwaheed@gmail.com>
2025-09-13 16:41:11 +0100

    Create db.js

---
+++ src/db.js
@@ -0,0 +1,24 @@
+import { MongoClient } from 'mongodb';
+
+const mongoUri = process.env.MONGODB_URI;
+if (!mongoUri) {
+    throw new Error("MONGODB_URI environment variable is not set.");
+}
+const mongoClient = new MongoClient(mongoUri);
+
+let db;
+
+export async function connectToDB() {
+    if (db) return { db };
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("bookkeeperDB");
+        console.log("✅ Successfully connected to MongoDB.");
+        const usersCollection = db.collection('users');
+        const transactionsCollection = db.collection('transactions');
+        return { db, usersCollection, transactionsCollection };
+    } catch (error) {
+        console.error("❌ Failed to connect to MongoDB:", error);
+        process.exit(1);
+    }
+}

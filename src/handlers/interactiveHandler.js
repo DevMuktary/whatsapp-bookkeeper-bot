edbf8958c8103158672d4947889046b47a157613
DevMuktary <mukhtarakanniabdulwaheed@gmail.com>
2025-10-15 15:24:34 +0100

    Update interactiveHandler.js

--- src/handlers/interactiveHandler.js
+++ src/handlers/interactiveHandler.js
@@ -2,7 +2,7 @@ import { findOrCreateUser, updateUserState } from '../db/userService.js';
 import { findTransactionById } from '../db/transactionService.js';
 import { findCustomerById } from '../db/customerService.js';
 import { generateInvoice } from '../services/pdfService.js';
-import { uploadMedia, sendDocument, sendTextMessage } from '../api/whatsappService.js';
+import { uploadMedia, sendDocument, sendTextMessage, sendInteractiveButtons } from '../api/whatsappService.js';
 import { USER_STATES, INTENTS } from '../utils/constants.js';
 import logger from '../utils/logger.js';
 import { executeTask } from './taskHandler.js';
@@ -10,37 +10,54 @@ import { ObjectId } from 'mongodb';
 
 export async function handleInteractiveMessage(message) {
     const whatsappId = message.from;
-    const interactiveData = message.interactive;
+    const interactive = message.interactive;
+    const user = await findOrCreateUser(whatsappId);
 
     try {
-        const user = await findOrCreateUser(whatsappId);
-
-        if (interactiveData.type !== 'button_reply') return;
-        
-        const buttonId = interactiveData.button_reply.id;
-
-        switch (user.state) {
-            case USER_STATES.AWAITING_BULK_PRODUCT_CONFIRMATION:
-                await handleBulkProductConfirmation(user, buttonId);
-                break;
-            case USER_STATES.AWAITING_INVOICE_CONFIRMATION:
-                await handleInvoiceConfirmation(user, buttonId);
-                break;
-            case USER_STATES.AWAITING_BANK_SELECTION_SALE:
-                await handleBankSelection(user, buttonId, INTENTS.LOG_SALE);
-                break;
-            case USER_STATES.AWAITING_BANK_SELECTION_EXPENSE:
-                await handleBankSelection(user, buttonId, INTENTS.LOG_EXPENSE);
-                break;
-            default:
-                logger.warn(`Received a button click in an unhandled state: ${user.state} for user ${whatsappId}`);
-                await sendTextMessage(whatsappId, "Sorry, I wasn't expecting that response right now.");
-                break;
+        if (interactive.type === 'button_reply') {
+            await handleButtonReply(user, interactive.button_reply.id);
+        } else if (interactive.type === 'list_reply') {
+            await handleListReply(user, interactive.list_reply.id);
         }
-
     } catch (error) {
         logger.error(`Error in interactive handler for ${whatsappId}:`, error);
         await sendTextMessage(whatsappId, "Something went wrong while processing your selection. Please try again.");
+    }
+}
+
+async function handleButtonReply(user, buttonId) {
+    switch (user.state) {
+        case USER_STATES.AWAITING_BULK_PRODUCT_CONFIRMATION:
+            await handleBulkProductConfirmation(user, buttonId);
+            break;
+        case USER_STATES.AWAITING_INVOICE_CONFIRMATION:
+            await handleInvoiceConfirmation(user, buttonId);
+            break;
+        case USER_STATES.AWAITING_BANK_SELECTION_SALE:
+            await handleBankSelection(user, buttonId, INTENTS.LOG_SALE);
+            break;
+        case USER_STATES.AWAITING_BANK_SELECTION_EXPENSE:
+            await handleBankSelection(user, buttonId, INTENTS.LOG_EXPENSE);
+            break;
+        case USER_STATES.AWAITING_DELETE_CONFIRMATION:
+            await handleDeleteConfirmation(user, buttonId);
+            break;
+        default:
+            logger.warn(`Received a button click in an unhandled state: ${user.state}`);
+            await sendTextMessage(user.whatsappId, "Sorry, I wasn't expecting that response right now.");
+            break;
+    }
+}
+
+async function handleListReply(user, listId) {
+    switch (user.state) {
+        case USER_STATES.AWAITING_TRANSACTION_SELECTION_FOR_DELETE:
+            await handleTransactionSelectionForDelete(user, listId);
+            break;
+        default:
+            logger.warn(`Received a list reply in an unhandled state: ${user.state}`);
+            await sendTextMessage(user.whatsappId, "Sorry, I wasn't expecting that response right now.");
+            break;
     }
 }
 
@@ -110,12 +127,49 @@ async function handleBankSelection(user, buttonId, originalIntent) {
     if (action === 'select_bank') {
         const transactionData = user.stateContext.transactionData;
         
-        // Add the selected bank ID to the data and execute the original task
         transactionData.linkedBankId = new ObjectId(bankIdStr);
 
         await sendTextMessage(user.whatsappId, "Great, linking this to your bank account...");
         await executeTask(originalIntent, user, transactionData);
     } else {
         logger.warn(`Unknown action in handleBankSelection: ${action}`);
+    }
+}
+
+async function handleTransactionSelectionForDelete(user, listId) {
+    const [action, txIdStr] = listId.split(':');
+    if (action !== 'select_tx_del') return;
+
+    const transaction = await findTransactionById(new ObjectId(txIdStr));
+    if (!transaction) {
+        await sendTextMessage(user.whatsappId, "I couldn't find that transaction. Please try again.");
+        await updateUserState(user.whatsappId, USER_STATES.IDLE);
+        return;
+    }
+
+    const formattedAmount = new Intl.NumberFormat('en-US', { style: 'currency', currency: user.currency }).format(transaction.amount);
+    const summary = `*Transaction Details:*\n\n*Type:* ${transaction.type}\n*Amount:* ${formattedAmount}\n*Description:* ${transaction.description}\n*Date:* ${new Date(transaction.date).toLocaleString()}`;
+    
+    await updateUserState(user.whatsappId, USER_STATES.AWAITING_DELETE_CONFIRMATION, { transactionId: transaction._id });
+    await sendTextMessage(user.whatsappId, summary);
+    await sendInteractiveButtons(user.whatsappId, "Are you sure you want to permanently delete this transaction? This action cannot be undone.", [
+        { id: 'confirm_delete', title: 'Yes, Delete It' },
+        { id: 'cancel_delete', title: 'No, Keep It' }
+    ]);
+}
+
+async function handleDeleteConfirmation(user, buttonId) {
+    if (buttonId === 'confirm_delete') {
+        const { transactionId } = user.stateContext;
+        if (!transactionId) {
+            await sendTextMessage(user.whatsappId, "I've lost track of which transaction to delete. Please start over.");
+            await updateUserState(user.whatsappId, USER_STATES.IDLE);
+            return;
+        }
+        await sendTextMessage(user.whatsappId, "Okay, deleting the transaction and reversing its effects... ⏳");
+        await executeTask(INTENTS.RECONCILE_TRANSACTION, user, { transactionId, action: 'delete' });
+    } else if (buttonId === 'cancel_delete') {
+        await sendTextMessage(user.whatsappId, "Okay, I've cancelled the deletion. The transaction has not been changed.");
+        await updateUserState(user.whatsappId, USER_STATES.IDLE);
     }
 }
